7dfd1e06c08478ff026fe5863647803a03ef9e71
svolya <svolja@mail.com>
2023-10-24 12:15:41 +0300

    vers24.10.22

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ print(("""
 - выбрать в какую валюту следует перевести
 - ввести количество исходной валюты
 """))
-def convert(amount, from_ticker, to_ticker,currencies):
+def convert(amount, from_ticker, to_ticker,currencies): # обслуживание расчета
     from_currency = currencies.get(from_ticker)
     to_currency = currencies.get(to_ticker)
 
@@ -20,15 +20,35 @@ def convert(amount, from_ticker, to_ticker,currencies):
     return round(amount * coefficient, 2)
 
 
-def input_currency(input_message, current_currencies):
+def input_currency(input_message, current_currencies):  # обслуживание ввода тикера валют
     ticker = input(f"{input_message}: ").upper()
 
     currency = current_currencies.get(ticker, None)
     if currency is None:
         print(f'ошибка при вводе валюты: {ticker}')
         exit()
-
     return ticker
+
+
+def input_amount():  # обслуживание ввода количества денег на конвертацию
+    while True:
+        try:
+            amount = float(input("введите количество валюты: "))
+          #  type(amount) != str
+            result = convert(amount, from_ticker, to_ticker, current_currencies)
+            print(f'Результат: {amount} {from_ticker} = {result} {to_ticker}')
+            go_back = int(input('Хотите продолжить? ДА = 1 / НЕТ = 2 :  '))
+            if go_back == 1:
+                input_amount()
+            else:
+                exit()
+
+        except ValueError:
+
+            print(f'Ошибка ввода!')
+            input_amount()
+        return
+
 current_currencies = get_currencies()
 
 # 3. Ввести исходную валюту
@@ -39,24 +59,6 @@ from_ticker = input_currency("введите исходную валюту ",
 to_ticker = input_currency("введите в какую  валюту перевести ",
                                         current_currencies)
 
-# 5. количество валюты для перевода
-def input_amount():
-    while True:
-        try:
-            amount = float(input("введите количество валюты: "))
-          #  type(amount) != str
-            result = convert(amount, from_ticker, to_ticker, current_currencies)
-            print(f'Результат: {amount} {from_ticker} = {result} {to_ticker}')
-            go_back = int(input('Хотите продолвить? ДА = 1 / НЕТ = 2 :  '))
-            if go_back == 1:
-                input_amount()
-            else:
-                exit()
-
-        except ValueError as error:
-
-            print(f'Ошибка ввода!')
-            input_amount()
 
 input_amount()
 
